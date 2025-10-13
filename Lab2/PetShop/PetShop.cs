7ed41636137fc6b239ca10399a0a280a374f40d8
Krzysztof Nasuta <krzysztof@nasuta.dev>
2025-10-13 15:52:53 +0200

    feat: add lab2

---
+++ Lab2/PetShop/PetShop.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace Training.DomainClasses
+{
+    public class PetShop
+    {
+        private IList<Pet> _petsInTheStore;
+
+        public PetShop(IList<Pet> petsInTheStore)
+        {
+            this._petsInTheStore = petsInTheStore;
+        }
+
+        public IEnumerable<Pet> AllPets()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                yield return pet;
+            }
+        }
+
+        public void Add(Pet newPet)
+        {
+            if(! this._petsInTheStore.Contains(newPet)) 
+                _petsInTheStore.Add(newPet);
+        }
+    }
+}
---
+++ Lab2a/PetShop/PetShop.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Training.DomainClasses
+{
+    public class PetShop
+    {
+        private IList<Pet> _petsInTheStore;
+
+        public PetShop(IList<Pet> petsInTheStore)
+        {
+            this._petsInTheStore = petsInTheStore;
+        }
+
+        public IEnumerable<Pet> AllPets()
+        {
+            return _petsInTheStore;
+        }
+
+        public void Add(Pet newPet)
+        {
+            if (!_petsInTheStore.Contains(newPet))
+            {
+                _petsInTheStore.Add(newPet);
+            }
+        }
+    }
+}
