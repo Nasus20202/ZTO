11afd7478a1b420ea3f725aadc89be302263b030
Krzysztof Nasuta <krzysztof@nasuta.dev>
2025-10-20 15:58:00 +0200

    feat: add lab 3a

---
+++ Lab3a/PetShop/PetShop.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Training.DomainClasses
+{
+    public class PetShop
+    {
+        private IList<Pet> _petsInTheStore;
+
+        public PetShop(IList<Pet> petsInTheStore)
+        {
+            this._petsInTheStore = petsInTheStore;
+        }
+
+        public IEnumerable<Pet> AllPets()
+        {
+            return new ReadOnlyOf<Pet>(_petsInTheStore);
+        }
+
+        public void Add(Pet newPet)
+        {
+            if(! this._petsInTheStore.Contains(newPet)) 
+                _petsInTheStore.Add(newPet);
+        }
+
+        public IEnumerable<Pet> AllCats()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species == Species.Cat)
+                    yield return pet;
+            }
+        }
+
+        public IEnumerable<Pet> AllPetsSortedByName()
+        {
+            var list = new List<Pet>(_petsInTheStore);
+            list.Sort((x, y) => string.Compare(x.name, y.name, StringComparison.Ordinal));
+            return list;
+        }
+    }
+}
